c3a559fc8b29a0e9d435b98ac2399ebeb02bce48
FLinss <Florian.linss@gmx.de>
2019-04-05 09:04:34 +0200

    Add correct whl version

---
+++ get_shapely_whl_version.py
@@ -0,0 +1,11 @@
+import platform
+import sys
+
+version = sys.version_info
+minor = version.minor
+major = version.major
+architecture = platform.architecture()[0][:2]
+ver = str(major) + str(minor)
+arch = '32' if int(architecture) == 32 else '_amd64'
+
+print("Shapely‑1.6.4.post1‑cp%s‑cp37m‑win%s.whl" % (ver, arch))
